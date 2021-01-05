89f3ff21ee2bf4fbcaaae1877092769c951cd460
Feliander <qazy777@gmail.com>
2021-01-05 20:02:39 +0300

    create template

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,31 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 User = get_user_model()
 
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
+
+
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Category name')
@@ -93,5 +118,4 @@ class Smartphone(Product):
     front_cam_mp = models.CharField(max_length=255, verbose_name='Front camera')
 
     def __str__(self):
-        return 'Customer" {}'.format(self.user.first_name, self.user.last_name)
-
+        return '{} : {}'.format(self.category.name, self.title)
